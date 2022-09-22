71b26643dbc74493a78442ab9b4f360e84dd88f8
Caleno-R <calenorose@gmail.com>
2022-09-22 11:33:18 +0300

    Remove the to do comments and API functions.

--- backend/src/api.py
+++ backend/src/api.py
@@ -35,9 +35,10 @@ def get_drinks():
 
     }), 200
 
+
 @app.route('/drinks-detail')
 @requires_auth('get:drinks-detail')
-def get_drinks_detail():
+def get_drinks_detail(payload):
     drinks = Drink.query.all()
 
     if len(drinks) == 0:
@@ -48,30 +49,35 @@ def get_drinks_detail():
         'drinks': [drink.long() for drink in drinks]
     }), 200
 
-app.route('/drinks', methods=['POST'])
+
+@app.route('/drinks', methods=['POST'])
 @requires_auth('post:drinks')
 def create_drink(payload):
     body = request.get_json()
+    print(body)
 
-    new_title = body.get('title', None)
-    new_recipe = body.get('recipe', None)
+    if not body:
+        abort(400)
+
+    title = body.get('title')
+    recipe = body.get('recipe')
 
     try:
-        # Create drink
-        drink = Drink(
-            title=new_title,
-            recipe=new_recipe,
-        )
-        #  Insert to db
-        drink.insert()
 
+        # Create drink
+        drink = Drink(title=title, recipe=json.dumps(recipe))
+        print(drink)
+
+        drink.insert()
+        
         return jsonify({
             'success': True,
-            'drinks': [drink.long() for drink in drink]
+            'drinks': drink.long()
         }), 200
 
     except:
-        abort(404)
+        abort(400)
+
 
 @app.route('/drinks/<int:id>', methods=['PATCH'])
 @requires_auth('patch:drinks')
@@ -105,27 +111,30 @@ def update_drinks(payload, id):
 
     }), 200
 
-@app.route('/delete/<int:id>', methods=['DELETE'])
+
+@app.route('/drinks/<int:id>', methods=['DELETE'])
 @requires_auth('delete:drinks')
 def delete_drinks(payload, id):
+
+    drink = Drink.query.filter(Drink.id == id).one_or_none()
+
+    if not drink:
+        abort(404)
+
     try:
-        drink = Drink.query.filter(Drink.id == id).one_or_none()
-
-        if drink is not None:
-            abort(404)
-
         drink.delete()
-
-        return jsonify({
-            'success': True,
-            'delete': id
-        }), 200
 
     except:
         abort(400)
 
+    return jsonify({
+        'success': True,
+        'delete': id
+    }), 200
 
 # Error Handling
+
+
 @app.errorhandler(422)
 def unprocessable(error):
     return jsonify({
@@ -134,19 +143,21 @@ def unprocessable(error):
         "message": "unprocessable"
     }), 422
 
+
 @app.errorhandler(400)
 def bad_request(error):
     return jsonify({
-        "success": False, 
-        "error": 400, 
+        "success": False,
+        "error": 400,
         "message": "bad request"
     }), 400
+
 
 @app.errorhandler(401)
 def unauthorized(error):
     return jsonify({
-        "success": False, 
-        "error": 401, 
+        "success": False,
+        "error": 401,
         "message": "unauthorized"
     }), 401
 
@@ -154,40 +165,43 @@ def unauthorized(error):
 @app.errorhandler(403)
 def forbidden(error):
     return jsonify({
-        "success": False, 
-        "error": 403, 
+        "success": False,
+        "error": 403,
         "message": "permission not allowed"
     }), 403
+
 
 @app.errorhandler(404)
 def not_found(error):
     return jsonify({
-        "success": False, 
-        "error": 404, 
+        "success": False,
+        "error": 404,
         "message": "resource not found"
     }), 404
+
 
 @app.errorhandler(405)
 def method_not_allowed(error):
     return jsonify({
-        "success": False, 
-        "error": 405, 
+        "success": False,
+        "error": 405,
         "message": "method not allowed"
     }), 405
+
 
 @app.errorhandler(500)
 def internal_server_error(error):
     return jsonify({
-        "success": False, 
-        "error": 500, 
+        "success": False,
+        "error": 500,
         "message": "internal server error"
     }), 400
+
 
 @app.errorhandler(AuthError)
 def auth_error(error):
     return jsonify({
-        "success": False, 
+        "success": False,
         "error": error.status_code,
         "message": error.error['description']
     }), error.status_code
-
